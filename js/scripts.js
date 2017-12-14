8b8e0287ee7c9eb67a9ed8e4e4a2dc27db921256
Amy Churchwell and Cheryl Frankenfield <cheryl@>
2017-12-14 10:48:16 -0800

    Add card append

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+//Backend Logic
+function Album (name, title, year, art) {
+  this.name = name;
+  this.title = title;
+  this.year = year;
+  this.art = art;
+};
+
+
+// Interface Logic
+
 $('document').ready(function(){
   $('#form').submit(function(event){
     var art = $('#art').val();
+    var name = $('#name').val();
+    var title = $('#title').val();
+    var year = $('#year').val();
+debugger;
+    var newAlbum = new Album(name, title, year, art);
 
     $('.gallery').append("<div class='card'></div>");
-    $('.card').css('background-image', 'url(' + art + ')');
+    $('.card:last-child').append("<div class='card-info'></div>");
+    $('.card:last-child').append("<div class='card-img'></div>");
+    $('.card:last-child .card-img').css('background-image', 'url(' + newAlbum.art + ')');
+    $('.card:last-child .card-info').append("<h2>" + newAlbum.name + "<br>" + newAlbum.title + "<br>" + newAlbum.year + "</h2>");
 
 
     event.preventDefault();
