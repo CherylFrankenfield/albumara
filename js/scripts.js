deb3f882caa8b3e612a6cb44af0ab3931e578b04
Amy Churchwell and Cheryl Frankenfield <amy@>
2017-12-14 14:40:39 -0800

    Add color,comments box and stars

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 //Backend Logic
-function Album (name, title, year, art) {
+function Album (name, title, year, art, comments) {
   this.name = name;
   this.title = title;
   this.year = year;
   this.art = art;
+  this.comments = comments;
 };
 
 
@@ -15,13 +16,14 @@ $('document').ready(function(){
     var name = $('#name').val();
     var title = $('#title').val();
     var year = $('#year').val();
-    var newAlbum = new Album(name, title, year, art);
+    var comments = $('#comments').val();
+    var newAlbum = new Album(name, title, year, art, comments);
 
     $('.gallery').append("<div class='card'></div>");
     $('.card:last-child').append("<div class='card-img'></div>");
     $('.card:last-child').append("<div class='card-info'></div>");
     $('.card:last-child .card-img').css('background-image', 'url(' + newAlbum.art + ')');
-    $('.card:last-child .card-info').append("<h2>" + newAlbum.name + "<br>" + newAlbum.title + "<br>" + newAlbum.year + "</h2>");
+    $('.card:last-child .card-info').append("<h2>" + newAlbum.name + "<br>" + newAlbum.title + "<br>" + newAlbum.year + "<br>" + newAlbum.comments + "</h2>");
 
 
     event.preventDefault();
